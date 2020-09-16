8f14b8a422d0c5cedd151df966b821567d72efc5
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-09-16 14:06:05 +0200

    review box setop
    boxes now have borders and backgrounds. and all content is placed correctly

--- app/js/main.js
+++ app/js/main.js
@@ -4,28 +4,28 @@ let allReviews = [
     foto:"./images/placeholder.jpg",
     occupation:"virksomhedsejer",
     stars:5,
-    discription:"det nye website blev lanceret i ffebruar 2018 og allerede inden for de første 6 måneder vaer konverteringerne steget mere end 150%."
+    discription:"det nye website blev lanceret i februar 2018 og allerede inden for de første 6 måneder vaer konverteringerne steget mere end 150%."
 },
 {
     name:"naven navensen",
     foto:"./images/placeholder.jpg",
     occupation:"virksomhedsejer",
     stars:5,
-    discription:"det nye website blev lanceret i ffebruar 2018 og allerede inden for de første 6 måneder vaer konverteringerne steget mere end 150%."
+    discription:"det nye website blev lanceret i februar 2018 og allerede inden for de første 6 måneder vaer konverteringerne steget mere end 150%."
 },
 {
     name:"naven navensen",
     foto:"./images/placeholder.jpg",
     occupation:"virksomhedsejer",
     stars:5,
-    discription:"det nye website blev lanceret i ffebruar 2018 og allerede inden for de første 6 måneder vaer konverteringerne steget mere end 150%."
+    discription:"det nye website blev lanceret i februar 2018 og allerede inden for de første 6 måneder vaer konverteringerne steget mere end 150%."
 },
 {
     name:"naven navensen",
     foto:"./images/placeholder.jpg",
     occupation:"virksomhedsejer",
     stars:5,
-    discription:"det nye website blev lanceret i ffebruar 2018 og allerede inden for de første 6 måneder vaer konverteringerne steget mere end 150%."
+    discription:"det nye website blev lanceret i februar 2018 og allerede inden for de første 6 måneder vaer konverteringerne steget mere end 150%."
 },
 ]
 console.log(allReviews)
@@ -37,10 +37,12 @@ function appendreviws(reviews){
 
         document.querySelector("#reviews").innerHTML+=`<article class="review">
         <div class="foto"><img src="${review.foto}"></div>
-        <div class="name">${review.name}</p></div>
+        <div class="review-background">
         <div class="stras"><p>${review.stras}</p></div>
+        <div class="name"><p>${review.name}</p></div>
         <div class="occupation"><p>${review.occupation}</p></div>
         <div class="discription"><p>${review.discription}</p></div>
+        </div>
         </article>
         `
     }
