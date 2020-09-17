33de37dacab0530403b8a2bde5fe305a1ca335a6
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-09-17 14:19:10 +0200

    arrow and border done
    added arrows for slide navagation.
    added border on review when focus

--- app/js/main.js
+++ app/js/main.js
@@ -59,7 +59,10 @@ function appendreviws(reviews){
         console.log(review)
         // here is how the content is setup for the html, and added in the div whit the id review
         document.querySelector("#reviews").innerHTML+=`<div class="review">
-        <div class="foto"><img src="${review.foto}"></div>
+        <div class="foto">
+        <div class="foto-background"></div>
+        <img src="${review.foto}">
+        </div>
         <div class="review-background">
         <div class="stras"><p>${review.stras}</p></div>
         <div class="name"><p>${review.name}</p></div>
@@ -76,7 +79,10 @@ appendreviws(allReviews);
 $(document).ready(function(){
     $('#reviews').slick({
         infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 3
-    })
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow:'<img class="slick_prev" src="images/prev.png">',
+        nextArrow:'<img class="slick_next" src="images/next.png">',
+        draggable:false,
+      });
 })
